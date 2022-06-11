fca452024ea584f4068f02273cd82318b8708775
Dior.Shelton <dior.shelton@gmail.com>
2022-06-10 22:28:33 -0400

    Update formatting of JSX and rename argument

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-const todoList = [{id:1, title:'Do Homework'}, {id:1, title:'Wash Car'}, {id:3, title:'Complete lesson 1.2 reading'}];
+const todoList = [
+  {id:1, title:'Do Homework'},
+  {id:1, title:'Wash Car'},
+  {id:3, title:'Complete lesson 1.2 reading'}
+];
 
 function App() {
   return (
     <div>
       <h1>To-Do List</h1>
       <ul>
-        {todoList.map(todo =>
-          <li key={todo.id}> 
-          {todo.title}</li>)}
+        {todoList.map(task => <li key={task.id}> {task.title}</li>)}
       </ul>
     </div>
   );
