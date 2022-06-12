3a2346b781100ca89a5ed12fd2ca8b7154515c1e
Dior.Shelton <dior.shelton@gmail.com>
2022-06-11 20:57:21 -0400

    Create component for ToDoList and refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-
-const todoList = [
-  {id:1, title:'Do Homework'},
-  {id:1, title:'Wash Car'},
-  {id:3, title:'Complete lesson 1.2 reading'}
-];
+import TodoList from './TodoList';
 
 function App() {
   return (
     <div>
       <h1>To-Do List</h1>
-      <ul>
-        {todoList.map(task => <li key={task.id}> {task.title}</li>)}
-      </ul>
+      <TodoList />
     </div>
   );
 }
