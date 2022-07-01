afc4646ea4967e20815b860ff6ce040e3eaef464
Dior.Shelton <dior.shelton@gmail.com>
2022-07-01 19:02:56 -0400

    Handle 'Add Todo Form' submit

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,11 +1,22 @@
 import React from "react";
+import {useState} from "react"
 
 const AddToDoForm = () => {
+
+	const handleAddToDo = (event) => {
+		event.preventDefault();
+		let todoTitle = event.target['title'].value;
+		console.log(todoTitle);
+		event.target.reset();
+	};
+
 	return (
-		<form>
+		<form onSubmit={handleAddToDo}>
 			<label htmlFor="todoTitle">Title </label>
-			<input type="text" id="todoTitle"></input>
-			<button type="submit">Add</button>
+			<input type="text" id="todoTitle" name="title"/>
+			<button type="submit">
+				Add
+			</button>
 		</form>
 	);
 };
