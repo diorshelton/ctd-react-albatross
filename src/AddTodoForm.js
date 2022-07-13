d95cb6691a2947fba97511d749a98578d13459e0
Dior.Shelton <dior.shelton@gmail.com>
2022-07-13 15:54:51 -0400

    Make formatting consistent between components

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -2,25 +2,29 @@ import React from "react";
 import { useState } from "react";
 
 const AddToDoForm = ({ onAddTodo }) => {
-	const [todoTitle, setTodoTitle] = useState('');
-	
+	const [todoTitle, setTodoTitle] = useState("");
+
 	const handleTitleChange = (event) => {
 		let newTodoTitle = event.target.value;
-		setTodoTitle(newTodoTitle)
-	}
+		setTodoTitle(newTodoTitle);
+	};
 	const handleAddToDo = (event) => {
 		event.preventDefault();
-		onAddTodo({title:todoTitle, id:Date.now});
+		onAddTodo({ title: todoTitle, id: Date.now });
 		setTodoTitle([]);
 	};
 
 	return (
 		<form onSubmit={handleAddToDo}>
 			<label htmlFor="todoTitle">Title </label>
-			<input type="text" id="todoTitle" name="title" value={todoTitle} onChange={handleTitleChange}/>
-			<button type="submit">
-				Add
-			</button>
+			<input
+				type="text"
+				id="todoTitle"
+				name="title"
+				value={todoTitle}
+				onChange={handleTitleChange}
+			/>
+			<button type="submit">Add</button>
 		</form>
 	);
 };
